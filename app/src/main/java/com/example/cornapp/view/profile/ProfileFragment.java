1bc9bed1bc620b5bb9a81a5d1c5070da4a526036
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2023-02-14 18:00:52 +0100

    BottomNavigation with fragments

---
+++ app/src/main/java/com/example/cornapp/view/profile/ProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.cornapp.view.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.cornapp.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+}
