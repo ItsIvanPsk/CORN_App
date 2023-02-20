ce01b377220f3dc321649ea224f719e1043ef4d8
Ivan Figueredo <ivanfigueredo.et@gmail.com>
2023-02-20 16:54:58 +0100

    Generate QR works

--- app/src/main/java/com/example/cornapp/view/payment/PaymentFragment.java
+++ app/src/main/java/com/example/cornapp/view/payment/PaymentFragment.java
@@ -1,5 +1,7 @@
 package com.example.cornapp.view.payment;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.cornapp.databinding.FragmentPaymentBinding;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 public class PaymentFragment extends Fragment {
 
@@ -17,7 +24,30 @@ public class PaymentFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentPaymentBinding.inflate(inflater, container, false);
+        binding.paymentQr.setImageBitmap(generateQRCode("prueba"));
         return binding.getRoot();
     }
+
+    public Bitmap generateQRCode(String text) {
+        int width = 800;
+        int height = 800;
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height, null);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
 
 }
--- app/src/main/java/com/example/cornapp/view/scan/ScanFragment.java
+++ app/src/main/java/com/example/cornapp/view/scan/ScanFragment.java
@@ -1,23 +1,63 @@
 package com.example.cornapp.view.scan;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.cornapp.databinding.FragmentScanBinding;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.example.cornapp.R;
+import com.google.zxing.Result;
 
 public class ScanFragment extends Fragment {
+    private CodeScanner mCodeScanner;
 
-    private FragmentScanBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentScanBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        final Activity activity = getActivity();
+        View root = inflater.inflate(R.layout.fragment_scan, container, false);
+        CodeScannerView scannerView = root.findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(activity, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                activity.runOnUiThread(() -> Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show());
+            }
+        });
+        scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 
 }
